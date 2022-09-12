f6d37b26ddcf421e8aecbfa81a4de95b6fe71a72
SindhuVardhan <112747707+SindhuVardhan@users.noreply.github.com>
2022-09-12 17:07:49 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main
+{
+   public static void main(String[] args)
+   {
+      int row, i, j, space, num;
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter no of rows:");
+      row=sc.nextInt();
+      
+      for(i=0; i<row; i++)
+      {
+         for(space=row; space>i; space--)
+         {
+            System.out.print(" ");
+         }
+         num=1;
+         for(j=0; j<=i; j++)
+         {
+            System.out.print(num+ " ");
+            num = num*(i-j)/(j+1);
+         }
+         System.out.print("\n");
+      }
+   }
+}
